c87d1227191f688300e80284d136f842a6d8635e
Christian Decker <decker.christian@gmail.com>
2018-11-15 11:31:19 +0100

    pip: Update version to 0.0.2 for pipy deploy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lnet",
-    version="0.0.1",
+    version="0.0.2",
     author="Christian Decker",
     author_email="decker.christian+pypi@gmail.com",
     description="Utilities to define and bootstrap lightning networks for testing",
